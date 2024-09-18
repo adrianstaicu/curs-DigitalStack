26cab1e769a37fc1c1074b4d5fbeb2279e00a684
unknown <staicuadrian88@gmail.com>
2024-09-18 21:37:09 +0300

    curs02

---
+++ curs02/curs_for.py
@@ -0,0 +1,8 @@
+#lista = [41, 42, 43]
+lista = 'Ana are mere'
+for i, v in enumerate(lista):
+    print(i, v)
+
+dictionar = {'a':1, 'b':2}
+for key, valuea in dictionar.items():
+    print(key, valuea)
---
+++ curs02/curs_while.py
@@ -0,0 +1,10 @@
+a = 10
+while 1 < a <=10:
+    print(f"Ana are {a} mere")
+    if a % 2 == 0:
+        continue
+# a = a - 1
+#  sau
+# a -= 1
+a -= 1
+    #break
